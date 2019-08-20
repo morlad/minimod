d9ac0f018294efd6a7ffd270771b3b6f759e73a5
morlad (iLeitgeb) <morlad@morlad.at>
2019-08-20 08:49:36 +0000

    More application of clang-format

--- src/netw.h
+++ src/netw.h
@@ -1,21 +1,18 @@
 // vi: noexpandtab tabstop=4 softtabstop=4 shiftwidth=0 list
 #pragma once
 
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
 struct netw_callbacks
 {
 	void (*completion)(
-		void const *in_udata,
-		void const *in_data,
-		size_t in_bytes,
-		int error);
-	void (*downloaded)(
-		void const *in_udata,
-		char const *path,
-		int error);
+	  void const *in_udata,
+	  void const *in_data,
+	  size_t in_bytes,
+	  int error);
+	void (*downloaded)(void const *in_udata, char const *path, int error);
 };
 
 bool
@@ -36,11 +33,11 @@ netw_get_request(char const *uri, char const *const headers[], void *udata);
 // send POST request. Calls *completion* callback when done.
 bool
 netw_post_request(
-	char const *uri,
-	char const *const headers[],
-	void const *body,
-	size_t nbody_bytes,
-	void *udata);
+  char const *uri,
+  char const *const headers[],
+  void const *body,
+  size_t nbody_bytes,
+  void *udata);
 
 // GET request writing response to temporary file.
 // Calls *downloaded* callback first, then *completion*.
@@ -49,7 +46,4 @@ netw_download(char const *in_uri, void *udata);
 
 // percent-encode *input*. Do not forget to free() the returned string.
 char *
-netw_percent_encode(
-	char const *input,
-	size_t len,
-	size_t *out_len);
+netw_percent_encode(char const *input, size_t len, size_t *out_len);
--- src/util.h
+++ src/util.h
@@ -8,34 +8,44 @@
 
 // get byte-size of file
 // returns -1 on error/if file does not exist
-int64_t fsu_fsize(char const *path);
+int64_t
+fsu_fsize(char const *path);
 
 // fopen() - wrapper that does 2 things:
 // 1) convert path (utf8) to wchar_t for windows to be happy.
 // 2) create missing directories (if any) of path, if mode contains 'w'
-FILE *fsu_fopen(char const *path, char const *mode);
+FILE *
+fsu_fopen(char const *path, char const *mode);
 
 // create directory - recursively up to the last '/'
-bool fsu_mkdir(char const *path);
+bool
+fsu_mkdir(char const *path);
 
 // move file
-bool fsu_mvfile(char const *from, char const *to, bool in_replace);
+bool
+fsu_mvfile(char const *from, char const *to, bool in_replace);
 
 // remove file
-bool fsu_rmfile(char const *path);
+bool
+fsu_rmfile(char const *path);
 
 // sleep in millseconds
-void sys_sleep(uint32_t ms);
+void
+sys_sleep(uint32_t ms);
 
 #ifdef _WIN32
 
 // wchar_t <-> utf8 conversion
-size_t sys_wchar_from_utf8(char const *in, wchar_t *out, size_t chars);
-size_t sys_utf8_from_wchar(wchar_t const *in, char *out, size_t bytes);
+size_t
+sys_wchar_from_utf8(char const *in, wchar_t *out, size_t chars);
+size_t
+sys_utf8_from_wchar(wchar_t const *in, char *out, size_t bytes);
 
 // add missing functions to win32
-int asprintf(char **strp, const char *fmt, ...);
-int vasprintf(char **strp, const char *fmt, va_list ap);
-#define strdup _strdup
+int
+asprintf(char **strp, const char *fmt, ...);
+int
+vasprintf(char **strp, const char *fmt, va_list ap);
+#	define strdup _strdup
 
 #endif
