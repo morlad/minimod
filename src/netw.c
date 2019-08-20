356550af94e81cc0b240786548bcf16f94fbf0ab
morlad (iLeitgeb) <morlad@morlad.at>
2019-08-20 08:05:56 +0000

    Apply clang-format to netw.c
    # Reason for this change?
    
    # How does it address the issue? This change...

--- src/netw.c
+++ src/netw.c
@@ -7,20 +7,19 @@
 static bool
 is_unreserved(char x)
 {
-  return (x >= 'A' && x <= 'Z')
-    || (x >= 'a' && x <= 'z')
-    || x == '-'
-    || x == '_'
-    || x == '.'
-    || x == '~';
+	// clang-format off
+	return (x >= 'A' && x <= 'Z')
+		|| (x >= 'a' && x <= 'z')
+		|| x == '-'
+		|| x == '_'
+		|| x == '.'
+		|| x == '~';
+	// clang-format on
 }
 
 
 char *
-netw_percent_encode(
-	char const *input,
-	size_t len,
-	size_t *out_len)
+netw_percent_encode(char const *input, size_t len, size_t *out_len)
 {
 	size_t nlen = 0;
 	for (size_t i = 0; i < len; ++i)
