a9c90ce85fd22da92339b662ba6cd70afe1786a8
morlad (iLeitgeb) <morlad@morlad.at>
2019-08-25 16:31:05 +0000

    Clean-up netw-macos.m

--- src/netw-macos.m
+++ src/netw-macos.m
@@ -21,31 +21,20 @@ struct netw
 static struct netw l_netw;
 
 
-struct task
+union netw_callback
 {
-	union
-	{
-		netw_request_callback request;
-		netw_download_callback download;
-	} callback;
-	void *udata;
-	NSMutableData *buffer;
-	FILE *file;
+	netw_request_callback request;
+	netw_download_callback download;
 };
 
 
-static struct task *
-alloc_task(void)
+struct task
 {
-	return calloc(1, sizeof(struct task));
-}
-
-
-static void
-free_task(struct task *task)
-{
-	free(task);
-}
+	union netw_callback callback;
+	void *userdata;
+	NSMutableData *buffer;
+	FILE *file;
+};
 
 
 static struct task *
@@ -75,7 +64,7 @@ task_from_dictionary(CFDictionaryRef in_dict, NSURLSessionTask *in_task)
 	{
 		assert(!task->file);
 		task->callback.request(
-		  task->udata,
+		  task->userdata,
 		  task->buffer.bytes,
 		  task->buffer.length,
 		  (int)((NSHTTPURLResponse *)nstask.response).statusCode);
@@ -85,14 +74,14 @@ task_from_dictionary(CFDictionaryRef in_dict, NSURLSessionTask *in_task)
 	{
 		assert(task->file);
 		task->callback.download(
-		  task->udata,
+		  task->userdata,
 		  task->file,
 		  (int)((NSHTTPURLResponse *)nstask.response).statusCode);
 	}
 
 	// clean up
 	CFDictionaryRemoveValue(l_netw.task_dict, nstask);
-	free_task(task);
+	free(task);
 }
 
 
@@ -108,7 +97,7 @@ task_from_dictionary(CFDictionaryRef in_dict, NSURLSessionTask *in_task)
 	}
 	else
 	{
-		assert(!task->file);
+		assert(task->buffer);
 		[task->buffer appendData:in_data];
 	}
 }
@@ -141,18 +130,19 @@ netw_deinit(void)
 }
 
 
-static NSString *l_verbs[] = { @"GET", @"POST", @"PUT", @"DELETE" };
+static NSString *const l_verbs[] = { @"GET", @"POST", @"PUT", @"DELETE" };
 
 
-bool
-netw_request(
+static bool
+netw_request_generic(
   enum netw_verb in_verb,
   char const *in_uri,
   char const *const headers[],
   void const *in_body,
   size_t in_nbytes,
-  netw_request_callback in_callback,
-  void *in_udata)
+  FILE *fout,
+  union netw_callback in_callback,
+  void *in_userdata)
 {
 	assert(in_uri);
 
@@ -184,16 +174,42 @@ netw_request(
 		nstask = [l_netw.session dataTaskWithRequest:request];
 	}
 
-	struct task *task = alloc_task();
-	task->udata = in_udata;
-	task->callback.request = in_callback;
-	task->buffer = [NSMutableData new];
+	struct task *task = calloc(1, sizeof *task);
+	task->userdata = in_userdata;
+	task->callback = in_callback;
+	task->file = fout;
+	if (!task->file)
+	{
+		task->buffer = [NSMutableData new];
+	}
 
 	CFDictionarySetValue(l_netw.task_dict, nstask, task);
 
 	[nstask resume];
 
 	return true;
+}
+
+
+bool
+netw_request(
+  enum netw_verb in_verb,
+  char const *in_uri,
+  char const *const headers[],
+  void const *in_body,
+  size_t in_nbytes,
+  netw_request_callback in_callback,
+  void *in_userdata)
+{
+	return netw_request_generic(
+		in_verb,
+		in_uri,
+		headers,
+		in_body,
+		in_nbytes,
+		NULL,
+		(union netw_callback){ .request = in_callback },
+		in_userdata);
 }
 
 
@@ -206,48 +222,15 @@ netw_download_to(
   size_t in_nbytes,
   FILE *fout,
   netw_download_callback in_callback,
-  void *in_udata)
+  void *in_userdata)
 {
-	assert(in_uri);
-	assert(fout);
-
-	NSString *uri = [NSString stringWithUTF8String:in_uri];
-	NSURL *url = [NSURL URLWithString:uri];
-
-	NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-
-	request.HTTPMethod = l_verbs[in_verb];
-
-	if (headers)
-	{
-		for (size_t i = 0; headers[i]; i += 2)
-		{
-			NSString *field = [NSString stringWithUTF8String:headers[i]];
-			NSString *value = [NSString stringWithUTF8String:headers[i + 1]];
-			[request setValue:value forHTTPHeaderField:field];
-		}
-	}
-
-	NSURLSessionDataTask *nstask;
-	if (in_body)
-	{
-		NSData *body = [NSData dataWithBytes:in_body length:in_nbytes];
-		nstask = [l_netw.session uploadTaskWithRequest:request fromData:body];
-	}
-	else
-	{
-		nstask = [l_netw.session dataTaskWithRequest:request];
-	}
-
-	struct task *task = alloc_task();
-	task->udata = in_udata;
-	task->callback.download = in_callback;
-	// this is the only difference to netw_request()
-	task->file = fout;
-
-	CFDictionarySetValue(l_netw.task_dict, nstask, task);
-
-	[nstask resume];
-
-	return true;
+	return netw_request_generic(
+		in_verb,
+		in_uri,
+		headers,
+		in_body,
+		in_nbytes,
+		fout,
+		(union netw_callback){ .download = in_callback },
+		in_userdata);
 }
